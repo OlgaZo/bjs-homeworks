b4ec9bafa73dbbb13b5948ff2d98f87b77a1b835
Olga Zosimova <olgazo@yandex.ru>
2019-09-17 22:25:27 +0300

    first commit Variables

---
+++ variables/main.js
@@ -0,0 +1,19 @@
+//Задание 1
+
+let algebra = 5;
+let geography = 4;
+let physics = 3;
+
+let averageScore = (algebra + geography + physics) / 3;
+console.log(averageScore);
+
+//Задание 2
+
+let name = "Olya";
+console.log(`Привет, мир! Меня зовут ${name}.`);
+
+//Задание 3
+
+let x = 2, y = 22, z = 0;
+let result = (x * y + 5 * z + x - 1);
+console.log(result);
