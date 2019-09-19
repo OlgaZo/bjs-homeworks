15d94878b593d6ccc976e06aecdfd71f76bcf02e
Olga Zosimova <olgazo@yandex.ru>
2019-09-19 23:33:41 +0300

    first commit wrappers-for-primitives

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,3 +1,5 @@
+'use srtict'
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -9,10 +11,23 @@ function calculateMortgage() {
     span.textContent = result;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+function calculateTotalMortgage(percent, contribution, amount, date) {
+    let totalAmount;
+    if (isNaN(Number(percent))) {
+        totalAmount = `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
+    } else if (isNaN(Number(amount))) {
+        totalAmount = `Параметр "Сумма кредита" содержит неправильное значение ${amount}`;
+    } else if (isNaN(Number(contribution))) {
+        totalAmount = `Параметр "Первоначальный взнос" содержит неправильное значение ${contribution}`;
+    } else {
+    let creditPeriod = ( (Number(date.substring(0,4)) - new Date().getFullYear() ) * 12) +  Number(date.substring(5,7)) - new Date().getMonth();
+    let creditSum = Number(amount) - Number(contribution);
+    let paymentMonth = creditSum * (percent / 1200 + percent  / 1200 / (((1 + percent / 1200) ** creditPeriod) - 1));
+    totalAmount = paymentMonth * creditPeriod;  
+    }
+    
+    return totalAmount;
 }
 
 function sayHello() {
@@ -23,6 +38,6 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+    greeting = (!!name === false) ? 'Привет, мир! Меня зовут Аноним.' : `Привет, мир! Меня зовут ${name}.`;
+    return greeting;    
 }
